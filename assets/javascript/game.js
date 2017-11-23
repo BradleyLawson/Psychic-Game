f9dc0330777127228bb6877f2d1e25f81f84d007
Brad Lawson <bradlawson@cfl.rr.com>
2017-11-23 14:48:47 -0500

    New saved JS files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,26 @@
-alert("Test");
+var wins = 0;
+var losses = 0;
+var maxGuesses = 10;
+var availCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","y","z"];
+var letter = availCharacters[Math.floor(Math.random() * availCharacters.length)];
+var guesses = [];
+
+
+document.onkeyup = function(event){
+	var userGuess = event.key;
+
+
+	if (userGuess !== letter) {
+		maxGuesses--;
+        document.querySelector("#guesses").innerHTML = userGuess;
+
+		//alert(userGuess);
+		// guess gets pushed into guesses[] array
+	}
+	else {
+        document.querySelector("#wins").innerHTML = ++wins;
+
+		//alert("wins: " + ++wins);
+	}
+
+}
